96ab9b8692a213c1b2fb66046206506c8870d46f
TedPham397 <thinhphamart@gmail.com>
2024-04-10 11:44:47 -0700

    beforemerge

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,15 +7,43 @@ const router = express.Router();
 
 //adding post request to sign up
 router.post('/signup', async (req,res) => {
+  
+  console.log('reqbody:', req.body);
+  
   try{
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const user = new User({ username: req.body.username, password: hashedPassword });
     await user.save();
     res.status(201).send({message: 'User created'});
   }catch{
-    res.status(500).send();
+    res.status(500).send({
+      username: req.body.username,
+      password: hashedPassword});
   }
 });
+
+/*
+ createStudent(req, res, next) {
+    console.log('Here!');
+    console.log('body', req.body);
+    const { firstName, lastName, age} = req.body;
+
+    Student.create({
+      firstName: firstName,
+      lastName: lastName,
+      age: age,}, (err, student) => {
+      if (err){ 
+        return next({
+          log: 'Express error handler caught StudentController middleware error in createStudednt',
+          status: 500,
+          message: { err: 'An error occurred' },
+        });
+      } 
+      res.locals.student = student;
+      return next();
+    });
+  },
+*/
 
 //adding post request to login
 router.post('/login', async (req,res) => {

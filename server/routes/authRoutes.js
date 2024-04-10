53725ba2a5d13b2263d78bd369fb8cd818a0ecc8
TedPham397 <thinhphamart@gmail.com>
2024-04-10 10:32:48 -0700

    merged with dev

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,50 +4,6 @@ import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 
 const router = express.Router();
-import cors from 'cors';
-import multer from 'multer';
-//multer variables
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-// eslint-disable-next-line no-unused-vars
-import receiptController from '../controllers/receiptControllers.js';
-import searchArray from '../controllers/searchArray.js';
-import memorize from '../controllers/memorize.js';
-
-// IMPORT MODEL INTO THE HERE OR CONTROLLER
-// THATMODEL.CREATE
-// THATMODEL.FIND
-
-router.use(
-  cors({
-    credentials: true,
-    origin: 'http://localhost:5173',
-  })
-);
-
-// post request
-//TODO: REVIEW THIS, WE MAY BE ABLE TO JUST REPLACE WITH MONGO DB
-//MODEL.CREATE(FILE)
-router.post('/upload', upload.single('file'), memorize, receiptController.uploadReceipt, 
-searchArray.searched, receiptController.saveReceipt, 
-(req, res) => res.status(200).json({
-  receiptArr: res.locals.array,
-  receipt: res.locals.receipt,
-}));
-
-// get request to get all receipts in collection
-router.get('/receipts', receiptController.getReceipts, (req, res) => {
-  res.status(200).json(res.locals.receiptArr);
-});
-
-// delete a receipt in collection
-router.delete('/receipts/:id', receiptController.deleteReceipt, (req, res) => {
-  res.status(200).json(res.locals.receipt)
-})
-
-// eslint-disable-next-line no-undef
-
-
 
 //adding post request to sign up
 router.post('/signup', async (req,res) => {
